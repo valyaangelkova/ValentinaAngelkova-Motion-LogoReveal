cbd6e0ee77fad932a5723e39db91cc10410732f3
Valentina Angelkova <vva27@drexel.edu>
2022-06-01 21:20:29 -0400

    added classes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,4 @@
-import { gsap } from "gsap";import { gsap } from "gsap";
+import { gsap } from "gsap";
 
 
 import { GSDevTools } from "gsap/GSDevTools";
@@ -15,11 +15,29 @@ const MainTL = gsap.timeline();
 
 function LettersFallIn (){
     let tl=gsap.timeline({});
-    tl.from("#b", {duration:1, y:-100, ease:"elastic.out"})
-    .from("l",{duration:1, y:-100, ease:"elastic.out"}) 
+
+    tl.set("#LOGO", {alpha:0})
+    .from("#b_2", {duration:1,alpha:0, y:-1000,  ease:"circ.out"})
+    .from("#l_2",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#a_3",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#c",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#k",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#s",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#a",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#n",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#d",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#l",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#i",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#b",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#r",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#a_2",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#r_2",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
+    .from("#y",{duration:1, alpha:0, y:-1000,  ease:"circ.out"},"-=0.75") 
     ;
     return tl;
 }
+
+function 
 MainTL
 .add(LettersFallIn())
 ;
